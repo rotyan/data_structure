0cbc5099611b0c65b4f8225c054bbe4a11be1b62
rotyan <rotyan@foxmail.com>
2020-11-30 10:40:43 +0800

    00

--- stack/stack1.cpp
+++ stack/stack1.cpp
@@ -2,16 +2,14 @@
 #include<stack>
 using namespace std;
 
-void clear(){
-  //  TOP==-1;
-}
+
 
 int main(){
     stack<int> st;
     for(int i=1;i<6;i++){
         st.push(i);
     }
-    clear();
+//    clear();
     printf("TOP==%d",st.top());
     getchar(); 
     return 0;
