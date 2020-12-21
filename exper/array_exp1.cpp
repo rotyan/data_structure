1888c515c05c1a1b70382227562625f1fb2e5366
rotyan <rotyan@foxmail.com>
2020-12-21 20:02:22 +0800

    12.21

--- exper/array_exp1.cpp
+++ exper/array_exp1.cpp
@@ -1,17 +1,17 @@
 //顺序表的逆序 
 #include <stdio.h>
-#define NUM 5
+#define NUM 20
 
 struct Array{
     int length; 
-    int data[NUM + 1];    //数据域
+    int data[NUM + 1];    //数据域，注意设置比原本长度大1，方便之后插入一个数值
 }L; 
 
 void exchange(){
 	int temp; 
     //主要的算法代码，进行交换
-     for(int i = 0; i < (L.length)/2; i++){
-         temp = L.data[i]; 
+    for(int i = 0; i < (L.length)/2; i++){
+         temp = L.data[i];      //中间变量temp存放要交换的数据
          L.data[i] = L.data[L.length-i-1]; 
          L.data[L.length-i-1] = temp; 
     }
@@ -23,17 +23,12 @@ void exchange(){
 }
 
 void input(){
-	for(int i = NUM; i < 3; i--){
-        L.data[i] = L.data[i-1];    //？为什么没用？水+1/水+2/水+3/水+4/水+5
+	for(int i = NUM; i > 3; i--){
+        L.data[i] = L.data[i-1];    //从L.data[1]之后所有数据往后移动一格
  	}
-/*
-    L.data[5] = L.data[4];
-    L.data[4] = L.data[3];
-    L.data[3] = L.data[2];
-*/
 	L.data[2] = 100; 
 	for(int i = 0; i < NUM + 1; i++){
-	printf("%d\t",L.data[i]);  
+	    printf("%d\t",L.data[i]);  //输出所有数据
 	}
 }
 
@@ -47,6 +42,5 @@ int main(){
     exchange(); 
     printf("\n所得的插入后为：\n"); 
     input(); 
-    getchar(); 
     return 0;
 }
