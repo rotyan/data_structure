f60f63fca11eccf68c44e51187a938d55c230a58
rotyan <rotyan@foxmail.com>
2020-12-30 22:19:06 +0800

    12.30

--- review/algorithm2.cpp
+++ review/algorithm2.cpp
@@ -8,7 +8,7 @@ struct  node{
 };
 
 node* Create(int array[]){
-    node *pre,*p,*head;
+    node *pre, *p, *head;
     head = new node;
     //头节点root没有数据域
     head->next = NULL;
@@ -22,14 +22,53 @@ node* Create(int array[]){
     }
     return head;
 }
-
-/*node* insert(node* root){
-    return;
-}*/
+//head为头节点地址，n为插入数据的位置，m为插入数据的值
+node* nodeInsert(node* head, int n, int m){
+    int count = 1;
+    node *pre, *p;
+    pre = head;
+    while(n != count){
+        pre = pre->next;
+        count++;
+    }
+    p = new node;
+    p->data = m;
+    p->next = pre->next;
+    pre->next = p;
+    return head;
+}
+//head为头节点地址，n为插入数据的位置
+node* nodeDelete(node* head, int n){
+    int count = 1;
+    node *pre;
+    pre = head;
+    while(n != count){
+        pre = pre->next;
+        count++;
+    }
+    pre->next = pre->next->next;
+    return head;
+}
+//head为头节点地址，m为查找数据的值
+node* nodeSearch(node* head, int m){    //头节点算不算位置1?还是0？
+    int count = 0;
+    node *pre, *p;
+    pre = head;
+    while(m != pre->data){
+        pre = pre->next;
+        count++;
+    }
+    printf("在第%d个位置",count);
+    return head;
+}
 
 int main(){
     int a[] = {123,234453,456,65};
     node* L = Create(a);
+    nodeInsert(L,3,1000);
+    nodeSearch(L,65);
+    printf("\n");
+    nodeDelete(L,3);
     L = L->next;
     while(L->next != NULL){
         printf("%d  ",L->data);
