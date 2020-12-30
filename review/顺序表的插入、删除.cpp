e0241a7a4af7e3ac6c8a967f4a38fd3c502bac0a
rotyan <rotyan@foxmail.com>
2020-12-30 20:34:16 +0800

    12.30

--- review/顺序表的插入、删除.cpp
+++ review/顺序表的插入、删除.cpp
@@ -1,26 +1,35 @@
 //顺序表的插入、删除
-//【自己的改进写法】但是没有考虑插入或删除时数组空间已经满了怎么办
 #include <stdio.h>
-#define NUM 4
+#define MAX 50   //数组最大空间
+#define NUM 4   //输入数据的数目
 
 struct array{
-    int data[NUM + 1];
-    int length; //善用这玩意
-}L;
+    int* data;
+    int length;
+};
+
+void Create(array &L){
+    L.data = new int[MAX];
+//    if (!L.data) exit(OVERFLOW);  //储存分配失败退出，不知为啥没用。。。
+    L.length = 0;
+}
 
 void listInsert(array &L, int j, int m){   //i为插入元素,m为插入位置（m-1为数组下标）,n为顺序表长度
-    if (m > L.length -1 || m < 0)
+    if (m > L.length + 1 || m < 1)
     printf("插入位置错误");
     return ;
-//    if (L.length == NUM) return;
+    if (L.length == MAX) 
+    printf("数组已满");
+    return;
     for (int i = L.length; i > m - 1; i--){
         L.data[i] = L.data[i - 1];
     }
     L.data[m - 1] = j;
+    L.length++;
 }
 
 void listDelete(array &L, int j, int m){   //i为删除元素,m为删除位置（m-1为数组下标）,n为顺序表长度
-    if (m > L.length -1 || m < 0)
+    if (m > L.length || m < 1)
     printf("删除位置错误");
     return ;
     for (int i = m - 1; i < L.length; i++){
@@ -30,19 +39,19 @@ void listDelete(array &L, int j, int m){   //i为删除元素,m为删除位置�
 }
 
 int main(){
-    int n;
-    L.length = 0; 
+    array L;
+    Create(L);
     for(int i = 0; i < NUM; i++){
         scanf("%d",&L.data[i]);
         L.length++;
     }
     listInsert(L, 2, 2);
-    for(int i = 0; i < L.length + 1; i++){
+    for(int i = 0; i < L.length; i++){
         printf("%d  ",L.data[i]);
     }
     printf("\n");
     listDelete(L, 2, 2);
-    for(int i = 0; i < L.length + 1; i++){
+    for(int i = 0; i < L.length; i++){
         printf("%d  ",L.data[i]);
     }
     printf("\n");
