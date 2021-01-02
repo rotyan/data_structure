9eb94fa06d589ac4875cc57370200707999a9cff
rotyan <rotyan@foxmail.com>
2021-01-02 17:33:56 +0800

    1.2

---
+++ review/algorithm5.cpp
@@ -0,0 +1,14 @@
+//邻接表建立无向图【没写，书上太复杂】
+#include<cstdio>
+const int MAX = 1000;
+const int INF = 10000000;
+
+#include <vector>
+using namespace std;
+vector<int> Adj[MAX];
+
+struct Node{
+    int v;  //边的终点编号
+    int w;  //边权
+    Node(int _v, int _w) : v(_v),w(_w) {}   //构造函数
+};
