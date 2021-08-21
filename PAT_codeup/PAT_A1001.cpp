27228f9155819ab83d654dea5b1cf7cca292a275
rotyan <rotyan@foxmail.com>
2021-08-21 22:30:23 +0800

    Site Updated:2021-08-21 22:30:22

--- PAT_codeup/PAT_A1001.cpp
+++ PAT_codeup/PAT_A1001.cpp
@@ -13,10 +13,10 @@ int main() {
     if (!sum) array[len++] == 0; //sum为0时特殊处理
     //将sum存入数组array[]中，低位对应低位
     while (sum) {
-        array[len++] = sum % 10;
+        array[len++] = sum % 10;        
         sum /= 10;
     }
-    for (int i = len - 1; i >= 0; i++){
+    for (int i = len - 1; i >= 0; i--){
         printf("%d", array[i]);
         if (i % 3 == 0) printf(",");
     }
