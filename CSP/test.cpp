d7d8b21bf99beaccc6b06774ef2ec79e478a48e5
rotyan <rotyan@foxmail.com>
2021-09-19 19:12:24 +0800

    Site Updated:2021-05-02 21:35:45

---
+++ CSP/test.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+    int n, m, L;
+    scanf("%d %d %d",&n,&m,&L);
+    int array[L] = {};
+    for (int i = 0; i < n*m - 1; i++){
+        while (){
+            
+        }
+    }
+    printf("%d", a);
+    return 0;
+}
